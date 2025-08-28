d1f9aecfa4be06d0ba6c55e6a986f54046096ff8
Umang Khemka <umangkhemka2005@gmail.com>
2025-08-28 14:57:07 +0530

    removed slash

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 
 
-app.use("/api/v1/users/",userRoutes);
+app.use("/api/v1/users",userRoutes);
 
 
 
